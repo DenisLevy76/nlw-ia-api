151dcbe743f9a9743f2a0ca61bd805a2cfa05d1c
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2023-09-13 00:07:34 -0300

    openai integration

---
+++ src/routes/generateAiCompletion.ts
@@ -0,0 +1,37 @@
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { prisma } from '../lib/prisma';
+import { openai } from '../lib/openai';
+
+export const generateAiCompletion = async (app: FastifyInstance) => {
+  app.post('/ai/complete', async (request, reply) => {
+    const bodySchema = z.object({
+      videoId: z.string().uuid(),
+      template: z.string(),
+      temperature: z.number().min(0).max(1).default(0.5)
+    })
+
+    const { template, temperature, videoId } = bodySchema.parse(request.body)
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId
+      }
+    })
+
+    if (!video.transcription) return reply.status(400).send({ error: 'Video transcription was not generated yet.' })
+
+    const promptMessage = template.replace('{transcription}', video.transcription)
+
+    const response = openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      temperature,
+      messages: [
+        { role: 'user', content: promptMessage }
+      ]
+    })
+
+
+    return response
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import { uploadVideo } from './routes/uploadVideo';
 
 import 'dotenv/config'
 import { createTranscription } from './routes/createTranscription';
+import { generateAiCompletion } from './routes/generateAiCompletion';
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideo)
 app.register(createTranscription)
+app.register(generateAiCompletion)
 
 app.listen({ port: 3333 }).then(() => console.log('Running...'))
